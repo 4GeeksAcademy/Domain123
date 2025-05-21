a5833f36c6306703fd64bca13f6cb2aff4fd76f4
SergioSanz94x <sergiosanz094@gmail.com>
2025-05-21 12:56:49 +0000

    dominiocreador

--- src/app.js
+++ src/app.js
@@ -5,7 +5,19 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
+  window.onload = function() {
+let pronoun = ["my", "yours", "secret"]
+let adj = ["awesome", "4Geeks", "stupid"]
+let noun = ["web", "Page", "portal"]
+let dom = [".com", ".es", ".org"]
+
+  let domain = 
+    pronoun[Math.floor(Math.random() * pronoun.length)] +
+    adj[Math.floor(Math.random() * adj.length)] +
+    noun[Math.floor(Math.random() * noun.length)] +
+    dom[Math.floor(Math.random() * dom.length)];
+
+     document.body.innerHTML = domain;
+
+     console.log("the generate domain:", domain);
+  };
